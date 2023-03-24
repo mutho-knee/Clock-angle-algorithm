54428f0fb5498ac4afd6aac34c494436847b21bc
mutho-knee <mwangimichelle911@gmail.com>
2023-03-24 09:46:20 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+hour = int(input("Enter the hour in 12 hour clock system\n"))
+min = int(input("Enter the minutes\n"))
+# Validating the time given
+if hour >12:
+    print("Enter valid hour")
+elif min > 60:
+    print("Enter valid minutes")
+else:
+    print("you've entered the time",hour,":",min)
+# Calculating the difference between the hour hand and minute hand
+angle = (((min/60) + hour) * 30) - (min/60 * 360)
+print("The angle difference between the hour hand and minute hand is", angle)
+
